1553f04018bb0434c7356b3884879c8faa512ccb
Mike Ryan <mike.ryan52@gmail.com>
2016-09-28 10:57:40 -0500

    fix(datetime): Serialize datetimes with UTC timestamps

--- src/util/datetime.ts
+++ src/util/datetime.ts
@@ -3,9 +3,9 @@ import * as moment from 'moment';
 const RFC_2445_DATETIME_FORMAT = 'YYYYMMDD HHmmss';
 
 export function parseDateTime(input: string): moment.Moment {
-  return moment.utc(input, RFC_2445_DATETIME_FORMAT);
+  return moment.utc(input);
 }
 
 export function serializeDateTime(input: moment.Moment): string {
-  return input.format(RFC_2445_DATETIME_FORMAT).replace(' ', 'T');
+  return input.format();
 }
